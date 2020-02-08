dacf6b8f012894efb5d2d5a5db0a56ed7adf1962
Icaro de Barros <icarodebarros@gmail.com>
2020-02-08 12:54:04 -0300

    Criados Repository e Service de Categoria

--- src/main/java/com/icarodebarros/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/icarodebarros/cursomc/resources/CategoriaResource.java
@@ -1,17 +1,24 @@
-package com.icarodebarros.cursomc.resources;
+ package com.icarodebarros.cursomc.resources;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.icarodebarros.cursomc.domain.Categoria;
+import com.icarodebarros.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaService service;
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public List<Categoria> listar() {
@@ -23,6 +30,12 @@ public class CategoriaResource {
 		lista.add(c2);
 		
 		return lista;
+	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = this.service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
