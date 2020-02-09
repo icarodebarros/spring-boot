5424246fd814c425565c9609ef3ae169618b2fdb
Icaro de Barros <icarodebarros@gmail.com>
2020-02-09 18:32:45 -0300

    Inserindo nova Categoria com POST

--- src/main/java/com/icarodebarros/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/icarodebarros/cursomc/resources/CategoriaResource.java
@@ -1,14 +1,15 @@
  package com.icarodebarros.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.icarodebarros.cursomc.domain.Categoria;
 import com.icarodebarros.cursomc.services.CategoriaService;
@@ -20,22 +21,18 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
-		
-		Categoria c1 = new Categoria(1, "Informática");
-		Categoria c2 = new Categoria(2, "Escritório");
-		List<Categoria> lista = new ArrayList<Categoria>();
-		lista.add(c1);
-		lista.add(c2);
-		
-		return lista;
-	}
-	
 	@RequestMapping(value="/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = this.service.buscar(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+					.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
