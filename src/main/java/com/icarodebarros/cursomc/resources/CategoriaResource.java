4d2f120a54a6fa23a0e0cce4e1e6c9a8307233d0
Icaro de Barros <icarodebarros@gmail.com>
2020-02-09 20:43:48 -0300

    Listado todas as Categorias com DTO

--- src/main/java/com/icarodebarros/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/icarodebarros/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
  package com.icarodebarros.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.icarodebarros.cursomc.domain.Categoria;
+import com.icarodebarros.cursomc.dto.CategoriaDTO;
 import com.icarodebarros.cursomc.services.CategoriaService;
 
 @RestController
@@ -46,5 +49,12 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		this.service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = this.service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
