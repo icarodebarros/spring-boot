942568daa63f0bd74e617e1ecb42bea13645ec60
Icaro de Barros <icarodebarros@gmail.com>
2020-02-09 18:45:05 -0300

    Atualizando Categoria com PUT, e ajustes

--- src/main/java/com/icarodebarros/cursomc/resources/ClienteResource.java
+++ src/main/java/com/icarodebarros/cursomc/resources/ClienteResource.java
@@ -1,8 +1,5 @@
  package com.icarodebarros.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,8 +18,8 @@ public class ClienteResource {
 	private ClienteService service;
 	
 	@RequestMapping(value="/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Cliente obj = this.service.buscar(id);
+	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
+		Cliente obj = this.service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
 
