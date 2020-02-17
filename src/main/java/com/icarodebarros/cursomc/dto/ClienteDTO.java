178ab0e9e2136678f791f66f173aad6b5c91945c
Icaro de Barros <icarodebarros@gmail.com>
2020-02-17 16:54:39 -0300

    Validacao customizada: email na repetido no update Cliente

--- src/main/java/com/icarodebarros/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/icarodebarros/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.icarodebarros.cursomc.domain.Cliente;
+import com.icarodebarros.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
