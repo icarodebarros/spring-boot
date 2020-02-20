3cafcf2a51897e83540c327f645e06dcd9b7c88c
Icaro de Barros <icarodebarros@gmail.com>
2020-02-20 15:34:27 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/icarodebarros/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/icarodebarros/cursomc/repositories/ClienteRepository.java
@@ -9,6 +9,6 @@ import com.icarodebarros.cursomc.domain.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
-	@Transactional(readOnly = true) // Melhora a performance
+	@Transactional(readOnly = true) // Melhora a performance indicando que é apenas uma consulta, não uma transação complexa
 	Cliente findByEmail(String email);
 }
---
+++ src/main/java/com/icarodebarros/cursomc/resources/utils/URL.java
@@ -0,0 +1,23 @@
+package com.icarodebarros.cursomc.resources.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class URL {
+	
+	public static String decodePram(String s) {
+		try {
+			return URLDecoder.decode(s, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			return "";
+		}
+	}
+	
+	public static List<Integer> decodeIntList(String s) {
+		return Arrays.asList(s.split(",")).stream().map(item -> Integer.parseInt(item)).collect(Collectors.toList());
+	}
+
+}
