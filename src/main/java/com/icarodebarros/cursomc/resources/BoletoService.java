f19b7591cbd1cf74a369644873b399e14cc45899
Icaro de Barros <icarodebarros@gmail.com>
2020-02-20 17:54:32 -0300

    Inserindo Pedido

---
+++ src/main/java/com/icarodebarros/cursomc/resources/BoletoService.java
@@ -0,0 +1,22 @@
+package com.icarodebarros.cursomc.resources;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import com.icarodebarros.cursomc.domain.PagamentoComBoleto;
+
+@Service
+public class BoletoService {
+	
+	public void peencherPagamentoComBoleto(PagamentoComBoleto pagto, Date instanteDoPedido) {
+		// OBS.: Numa situação real deveria ter uma chamada à um webService que geraria o boleto,
+		// e a data de vencimento do mesmo seria salva aqui
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(instanteDoPedido);
+		cal.add(Calendar.DAY_OF_MONTH, 7);
+		pagto.setDataVencimento(cal.getTime());
+	}
+
+}
